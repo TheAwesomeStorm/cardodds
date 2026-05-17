9f9b04026a61a69679a7fb9ba99b475223f68bae
Daniel Mendes Sechinske <dsechinske@mobiltec.com.br>
2026-05-17 11:58:33 -0300

    Add CLI argument parsing with clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,2 @@
 pub mod hypergeometric;
 pub mod input;
-
-    
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use anyhow::Result;
 use cardodds::hypergeometric::probability_at_least_one;
 use cardodds::input::{DeckSize, DrawCount, SourceCount};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about = "Calculate hypergeometric probabilities for card games")]
+struct Args {
+    #[arg(long, help = "Total cards in the deck")]
+    deck: Option<u64>,
+
+    #[arg(long, help = "Number of cards drawn")]
+    draw: Option<u64>,
+
+    #[arg(long, help = "Number of source cards in the deck")]
+    sources: Option<u64>,
+}
 
 fn main() -> Result<()> {
-    let deck = DeckSize::new(60)?;
-    let draw = DrawCount::new(7, &deck)?;
-    let sources = SourceCount::new(4, &deck)?;
+    let args = Args::parse();
+
+    let deck = DeckSize::new(args.deck.unwrap_or(60))?;
+    let draw = DrawCount::new(args.draw.unwrap_or(7), &deck)?;
+    let sources = SourceCount::new(args.sources.unwrap_or(4), &deck)?;
 
     let prob = probability_at_least_one(&deck, &draw, &sources);
     println!("{:.4}", prob);
